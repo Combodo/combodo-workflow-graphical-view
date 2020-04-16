0d56accab8a7009573103bb2746603552b978e99
Molkobain <lajarige.guillaume@free.fr>
2020-04-16 10:18:48 +0200

    :globe_with_meridians: Improve french translations

--- fr.dict.combodo-workflow-graphical-view.php
+++ fr.dict.combodo-workflow-graphical-view.php
@@ -8,7 +8,7 @@
 Dict::Add('FR FR', 'French', 'Français', array(
 	'workflow-graphical-view:UI:Button:ShowLifecycle' => 'Voir le cycle de vie',
 	'workflow-graphical-view:UI:Modal:Title' => 'Cycle de vie',
-	'workflow-graphical-view:UI:Legend:Item:CurrentState' => 'Etat courant',
+	'workflow-graphical-view:UI:Legend:Item:CurrentState' => 'État courant',
 	'workflow-graphical-view:UI:Legend:Item:UserStimuli' => 'Transitions utilisateurs',
 	'workflow-graphical-view:UI:Legend:Item:InternalStimuli' => 'Transitions internes',
 ));
