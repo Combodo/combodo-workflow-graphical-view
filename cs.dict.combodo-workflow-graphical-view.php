a30e206a334c6e26cbc22e6a4d3f5bfc83d7fe2f
Timothee <timothee.callet@combodo.com>
2024-06-11 16:59:00 +0200

    Automated dictionaries synchronization with english reference

--- cs.dict.combodo-workflow-graphical-view.php
+++ cs.dict.combodo-workflow-graphical-view.php
@@ -1,27 +1,20 @@
 <?php
 /**
- * Copyright (C) 2013-2024 Combodo SAS
+ * Localized data
  *
- * This file is part of iTop.
- *
- * iTop is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * iTop is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
+ * @copyright Copyright (C) 2010-2024 Combodo SAS
+ * @license    https://opensource.org/licenses/AGPL-3.0
+ * 
  */
-Dict::Add('CS CZ', 'Czech', 'Čeština', array(
-	'workflow-graphical-view:UI:Button:ShowLifecycle' => 'Show workflow~~',
-	'workflow-graphical-view:UI:Modal:Title' => 'Workflow~~',
-	'workflow-graphical-view:UI:Legend:Item:CurrentState' => 'Current state~~',
-	'workflow-graphical-view:UI:Legend:Item:UserStimuli' => 'User stimuli~~',
-	'workflow-graphical-view:UI:Legend:Item:InternalStimuli' => 'Internal stimuli~~',
-	'workflow-graphical-view:Error:NoStateAttribute' => 'Cannot generate lifecycle graph for %1$s as it has no state attribute.~~',
+/**
+ *
+ */
+Dict::Add('CS CZ', 'Czech', 'Čeština', [
 	'workflow-graphical-view:Error:GraphVizGeneration' => 'Whoops! Lifecycle could not be generated, check the error log for more information.~~',
-));
+	'workflow-graphical-view:Error:NoStateAttribute' => 'Cannot generate lifecycle graph for %1$s as it has no state attribute.~~',
+	'workflow-graphical-view:UI:Button:ShowLifecycle' => 'Show workflow~~',
+	'workflow-graphical-view:UI:Legend:Item:CurrentState' => 'Current state~~',
+	'workflow-graphical-view:UI:Legend:Item:InternalStimuli' => 'Internal stimuli~~',
+	'workflow-graphical-view:UI:Legend:Item:UserStimuli' => 'User stimuli~~',
+	'workflow-graphical-view:UI:Modal:Title' => 'Workflow~~',
+]);
--- de.dict.combodo-workflow-graphical-view.php
+++ de.dict.combodo-workflow-graphical-view.php
@@ -1,27 +1,20 @@
 <?php
 /**
- * Copyright (C) 2013-2024 Combodo SAS
+ * Localized data
  *
- * This file is part of iTop.
- *
- * iTop is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * iTop is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
+ * @copyright Copyright (C) 2010-2024 Combodo SAS
+ * @license    https://opensource.org/licenses/AGPL-3.0
+ * 
  */
-Dict::Add('DE DE', 'German', 'Deutsch', array(
-	'workflow-graphical-view:UI:Button:ShowLifecycle' => 'Ablauf anzeigen',
-	'workflow-graphical-view:UI:Modal:Title' => 'Ablauf',
-	'workflow-graphical-view:UI:Legend:Item:CurrentState' => 'Aktueller Status',
-	'workflow-graphical-view:UI:Legend:Item:UserStimuli' => 'Benutzer-Statusübergänge',
-	'workflow-graphical-view:UI:Legend:Item:InternalStimuli' => 'Interne Statusübergänge',
-	'workflow-graphical-view:Error:NoStateAttribute' => 'Es kann kein Lebenszyklusdiagramm für %1$s erstellt werden, da es kein Statusattribut hat.',
+/**
+ *
+ */
+Dict::Add('DE DE', 'German', 'Deutsch', [
 	'workflow-graphical-view:Error:GraphVizGeneration' => 'Hoppla! Der Lebenszyklus konnte nicht erstellt werden. Überprüfen Sie das Error-Log für weitere Informationen.',
-));
+	'workflow-graphical-view:Error:NoStateAttribute' => 'Es kann kein Lebenszyklusdiagramm für %1$s erstellt werden, da es kein Statusattribut hat.',
+	'workflow-graphical-view:UI:Button:ShowLifecycle' => 'Ablauf anzeigen',
+	'workflow-graphical-view:UI:Legend:Item:CurrentState' => 'Aktueller Status',
+	'workflow-graphical-view:UI:Legend:Item:InternalStimuli' => 'Interne Statusübergänge',
+	'workflow-graphical-view:UI:Legend:Item:UserStimuli' => 'Benutzer-Statusübergänge',
+	'workflow-graphical-view:UI:Modal:Title' => 'Ablauf',
+]);
